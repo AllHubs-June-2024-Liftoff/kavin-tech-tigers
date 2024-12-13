465666fcce1a286b7b614687d25982698520b5c1
Douglas Imler <Douglas.Imler@gmail.com>
2024-12-13 01:26:24 -0600

    Project Name Spelled Correctly, Hello World Achieved

---
+++ src/main/java/TechTigers/BicycleBuddies/BicycleBuddiesApplication.java
@@ -0,0 +1,13 @@
+package TechTigers.BicycleBuddies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleBuddiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicycleBuddiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TechTigers/BicycleBuddies/BicycleBuddiesApplicationTests.java
@@ -0,0 +1,13 @@
+package TechTigers.BicycleBuddies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleBuddiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
