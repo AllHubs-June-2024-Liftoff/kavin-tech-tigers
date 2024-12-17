efea581d31dd90b683cb8436a2b57a83beb87731
Eric S <estriler@hotmail.com>
2024-12-17 17:10:30 -0600

    commented out nonworking code

--- src/main/java/TechTigers/BicycleBuddies/Controllers/AuthenticationController.java
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/AuthenticationController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class AuthenticationController {
 
-    @Autowired
-    private UserRepository userRepository;
+//    @Autowired
+//    private UserRepository userRepository;
 
     @GetMapping("/register")
     public String displayRegistrationForm(Model model){
