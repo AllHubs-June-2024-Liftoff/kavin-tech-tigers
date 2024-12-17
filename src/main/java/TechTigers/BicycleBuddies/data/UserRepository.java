8ab4f173f9d77717a33c9b81cd68870e6a665443
Eric S <estriler@hotmail.com>
2024-12-17 11:52:26 -0600

    added UserRepository

---
+++ src/main/java/TechTigers/BicycleBuddies/data/UserRepository.java
@@ -0,0 +1,11 @@
+package TechTigers.BicycleBuddies.data;
+
+import TechTigers.BicycleBuddies.models.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository extends CrudRepository<User, Integer>{
+
+    User findbyUsername(String username);
+
+}
