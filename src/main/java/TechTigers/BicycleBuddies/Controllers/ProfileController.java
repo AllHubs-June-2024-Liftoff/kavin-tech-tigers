925e787c9971b1e7de10571e947f9c7b79cd7564
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-12-21 00:14:41 -0500

    fixed ProfileController

--- src/main/java/TechTigers/BicycleBuddies/Controllers/ProfileController.java
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/ProfileController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("profile-edit")
+@RequestMapping("/profile-edit")
 public class ProfileController {
 
-    @GetMapping("/")
+    @GetMapping()
     public String mainPage() {
         return "profile-edit";
     } //load mock edit page edit
