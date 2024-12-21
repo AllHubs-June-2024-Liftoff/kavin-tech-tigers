42eab9ac725ac67436377100a00bdd2e5b20802d
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-12-20 23:54:02 -0500

    worked on profile CRUD/Service package & Controller

--- src/main/java/TechTigers/BicycleBuddies/Controllers/ProfileController.java
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/ProfileController.java
@@ -2,10 +2,16 @@ package TechTigers.BicycleBuddies.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("profile-edit")
 public class ProfileController {
 
-    @GetMapping("profile-edit")
-    public String profileEdit(){ return "profile-edit";}
+    @GetMapping("/")
+    public String mainPage() {
+        return "profile-edit";
+    } //load mock edit page edit
+
+
 }
