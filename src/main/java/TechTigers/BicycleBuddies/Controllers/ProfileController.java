3e1ab2b267282627c17ed219d7e877622dc22050
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-12-26 16:02:17 -0500

    controller update

--- src/main/java/TechTigers/BicycleBuddies/Controllers/ProfileController.java
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/ProfileController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProfileController {
 
     @GetMapping()
-    public String mainPage() {
+    public String profileEdit() {
         return "profile-edit";
     } //load mock edit page edit
 
