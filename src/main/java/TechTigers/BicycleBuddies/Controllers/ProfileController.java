5662d0179546c785d7f88bc8f943cadc1c4d9f87
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-12-27 19:45:43 -0500

    fixed controller & added profile view

--- src/main/java/TechTigers/BicycleBuddies/Controllers/ProfileController.java
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/ProfileController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/profile-edit")
+@RequestMapping("")
 public class ProfileController {
+    @GetMapping
+    public String profileView(){return "profile";}// load mock profile view
 
     @GetMapping()
     public String profileEdit() {
