cb9aacb1575d26850789c28c2d8c1c5daac11a58
Eric S <estriler@hotmail.com>
2024-12-30 17:47:26 -0600

    added css to center items

--- src/main/java/TechTigers/BicycleBuddies/data/UserRepository.java
+++ src/main/java/TechTigers/BicycleBuddies/data/UserRepository.java
@@ -3,7 +3,7 @@ package TechTigers.BicycleBuddies.data;
 //import TechTigers.BicycleBuddies.models.User;
 //import org.springframework.data.repository.CrudRepository;
 //import org.springframework.stereotype.Repository;
-
+//
 //@Repository
 //public interface UserRepository extends CrudRepository<User, Integer>{
 //
