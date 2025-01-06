0eba4d6007ce26450853db9daf196783311ef7b5
Douglas Imler <Douglas.Imler@gmail.com>
2025-01-05 18:23:31 -0600

    Obscured API key, Added Bike trails to map

--- src/main/java/TechTigers/BicycleBuddies/Controllers/MapController.java
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/MapController.java
@@ -1,13 +1,22 @@
 package TechTigers.BicycleBuddies.Controllers;
 
+import TechTigers.BicycleBuddies.models.data.ConfigRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("map")
 public class MapController {
 
-    @GetMapping("/map")
-    public String showMap(){
+    @Autowired
+    private ConfigRepository configRepository;
+
+    @GetMapping("")
+    public String showMap(Model model){
+        model.addAttribute("mapsApi", configRepository.findById(1).get());
         return "map";
     }
 }
