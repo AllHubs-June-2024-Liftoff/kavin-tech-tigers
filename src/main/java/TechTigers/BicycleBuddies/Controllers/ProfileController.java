05ef93c7da795e738bcd4453841994f81b66a4a3
Erica V <132383093+ericavalera@users.noreply.github.com>
2025-01-08 12:49:42 -0500

    Added titles to Controller and Profile/Profile-Edit View

--- src/main/java/TechTigers/BicycleBuddies/Controllers/ProfileController.java
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/ProfileController.java
@@ -1,18 +1,24 @@
 package TechTigers.BicycleBuddies.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProfileController {
+    //@Autowired
+    // private ProfileRepository profileRepository
+
     @GetMapping("/profile")
-    public String profileView(){return "profile";}// load mock profile view
+    public String profileView(Model model){
+        model.addAttribute("title", "User Profile");
+        return "profile";}// load mock profile view
 
     @GetMapping("/profile-edit")
-    public String profileEdit() {
-        return "profile-edit";
-    } //load mock edit page edit
+    public String profileEdit(Model model) {
+        model.addAttribute("title", "Profile Edit");
+        return "profile-edit";} //load mock edit page edit
 
 
 }
