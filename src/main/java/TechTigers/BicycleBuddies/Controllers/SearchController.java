78683dc490a383fbaf639c3896fe962c8b8ffbbd
Eric S <estriler@hotmail.com>
2025-01-15 19:19:24 -0600

    centered list items on search for friends page

--- src/main/java/TechTigers/BicycleBuddies/Controllers/SearchController.java
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/SearchController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("search")
+@RequestMapping("friends/search")
 public class SearchController {
 
     @Autowired
@@ -31,7 +31,6 @@ public class SearchController {
         } else {
             users = UserData.findByValue(searchTerm, userRepository.findAll());
         }
-        model.addAttribute("title", "Results");
         model.addAttribute("users", users);
 
         return "friends/search";
