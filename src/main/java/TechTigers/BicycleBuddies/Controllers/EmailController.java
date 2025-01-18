2724e5769b061c56a9f5fdcf34d953ae44e6a226
Eric S <estriler@hotmail.com>
2025-01-18 13:04:46 -0600

    added EmailController

---
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/EmailController.java
@@ -0,0 +1,18 @@
+package TechTigers.BicycleBuddies.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EmailController {
+
+//    @Autowired
+//    private JavaMailSender mailSender;
+
+    @RequestMapping("/send-email")
+    public String sendEmail(){
+
+    }
+
+}
