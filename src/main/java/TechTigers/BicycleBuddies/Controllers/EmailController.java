74ff7b844ec9553f27edf11fa675413994d55a28
Eric S <estriler@hotmail.com>
2025-01-18 13:59:45 -0600

    set up basic email sender with bugs

--- src/main/java/TechTigers/BicycleBuddies/Controllers/EmailController.java
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/EmailController.java
@@ -1,17 +1,36 @@
 package TechTigers.BicycleBuddies.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class EmailController {
 
-//    @Autowired
-//    private JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
+
+    public EmailController(JavaMailSender mailSender){
+        this.mailSender = mailSender;
+    }
 
     @RequestMapping("/send-email")
     public String sendEmail(){
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+
+            message.setFrom("bicyclebuddies8080@gmail.com");
+            message.setTo("bicyclebuddies8080@gmail.com");
+            message.setSubject("Please Verify Your Account");
+            message.setText("Test Email");
+
+            mailSender.send(message);
+            return "success";
+        } catch (Exception e){
+            return e.getMessage();
+        }
 
     }
 
