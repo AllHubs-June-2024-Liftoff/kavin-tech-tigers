3d7f7b350ad4f6efebe416d057bd3278840ff139
Eric S <estriler@hotmail.com>
2025-01-20 11:40:54 -0600

    added verificationCode field in User class as well as generateToken method

--- src/main/java/TechTigers/BicycleBuddies/models/User.java
+++ src/main/java/TechTigers/BicycleBuddies/models/User.java
@@ -3,6 +3,8 @@ package TechTigers.BicycleBuddies.models;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
 import jakarta.persistence.Entity;
 import javax.validation.constraints.NotNull;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +17,8 @@ public class User extends AbstractEntity{
 
     @NotNull
     private String pwHash;
+
+    private int verificationCode = generateToken();
 
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
@@ -31,6 +35,15 @@ public class User extends AbstractEntity{
 
     public boolean isMatchingPassword(String password){
         return encoder.matches(password, pwHash);
+    }
+
+    public int getVerificationCode() {
+        return verificationCode;
+    }
+
+    public static int generateToken(){
+        Random generator = new Random();
+        return generator.nextInt(900000) + 100000;
     }
 
 }
