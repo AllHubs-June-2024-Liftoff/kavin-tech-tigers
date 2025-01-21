9b150500bd066e249166042188f708ebc43fb2af
Eric S <estriler@hotmail.com>
2025-01-21 14:59:44 -0600

    created SMSRestController

---
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/SMSRestController.java
@@ -0,0 +1,9 @@
+package TechTigers.BicycleBuddies.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class SMSRestController {
+}
