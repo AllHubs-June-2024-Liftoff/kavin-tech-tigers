ea8d7da30b6e07770e2f37e2650957114a78bed2
Eric S <estriler@hotmail.com>
2025-01-21 18:53:29 -0600

    added SMSSendRequest model and SMSService service

--- src/main/java/TechTigers/BicycleBuddies/Controllers/SMSRestController.java
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/SMSRestController.java
@@ -1,9 +1,29 @@
 package TechTigers.BicycleBuddies.Controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import TechTigers.BicycleBuddies.models.SMSSendRequest;
+import TechTigers.BicycleBuddies.service.SMSService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import static TechTigers.BicycleBuddies.models.User.generateToken;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/api/v1")
 public class SMSRestController {
+
+    @Autowired
+    SMSService smsService;
+
+    @GetMapping("/processSMS")
+    public String processSMS(){
+        return "todo";
+    }
+
+    @PostMapping("/processSMS")
+    public String processSMS(@RequestBody SMSSendRequest sendRequest){
+        sendRequest.setSmsMessage("Your login code is " + generateToken());
+        sendRequest.setDestinationSMSNumber("+16363687378");
+        return smsService.sendSMS(sendRequest.getDestinationSMSNumber(), sendRequest.getSmsMessage());
+    }
+
 }
