7509bb3e0fbafe74a432f6d6596271db7e7d49a4
Eric S <estriler@hotmail.com>
2025-01-22 21:39:53 -0600

    commented out SMSService cuz its buggy and not used atm

---
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/SMSController.java
@@ -0,0 +1,35 @@
+package TechTigers.BicycleBuddies.Controllers;
+
+import TechTigers.BicycleBuddies.models.SMSSendRequest;
+//import TechTigers.BicycleBuddies.service.SMSService;
+import org.springframework.web.bind.annotation.*;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+
+import static TechTigers.BicycleBuddies.models.User.generateToken;
+
+@RestController
+@RequestMapping("sms-verification")
+public class SMSController {
+
+//    @Autowired
+//    SMSService smsService;
+
+    @GetMapping("")
+    public String sendSMS(@RequestBody SMSSendRequest sendRequest){
+        final String ACCOUNT_SID = System.getenv("twilio_account_sid");
+        final String AUTH_TOKEN = System.getenv("twilio_auth_token");
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        //First phone number is the TO the second phone number is the FROM
+        Message message = Message.creator(new com.twilio.type.PhoneNumber("+16363687378"),
+                        new com.twilio.type.PhoneNumber("+16363687378"),
+                        "Your login code is " + generateToken())
+                        .create();
+        return message.getBody();
+//        sendRequest.setSmsMessage("Your login code is " + generateToken());
+//        sendRequest.setDestinationSMSNumber("+16363687378");
+//        return smsService.sendSMS(sendRequest.getDestinationSMSNumber(), sendRequest.getSmsMessage());
+    }
+
+}
