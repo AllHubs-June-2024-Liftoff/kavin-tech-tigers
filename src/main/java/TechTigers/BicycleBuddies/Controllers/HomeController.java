801261c12f1c8be3da2420553ec6328e4c81ac86
Eric S <estriler@hotmail.com>
2025-01-27 16:37:55 -0600

    added authenticationFilter

--- src/main/java/TechTigers/BicycleBuddies/Controllers/HomeController.java
+++ src/main/java/TechTigers/BicycleBuddies/Controllers/HomeController.java
@@ -1,23 +1,54 @@
 package TechTigers.BicycleBuddies.Controllers;
 
+import TechTigers.BicycleBuddies.data.UserRepository;
+import TechTigers.BicycleBuddies.models.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String index(){
-        return "index";
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session){
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null){
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if(user.isEmpty()){
+            return null;
+        }
+
+        return user.get();
     }
 
-    @GetMapping("/home-logged-out")
+    private static void setUserInSession(HttpSession session, User user){
+        session.setAttribute(userSessionKey, user.getId());
+    }
+
+    @GetMapping("")
+    public String index(HttpServletRequest request){
+
+        if(request.getSession() == null){
+            return "login";
+        } else if (getUserFromSession(request.getSession())) {
+            return "home";
+        }
+    }
+
+    @GetMapping("/home")
     public String homePageLoggedOut(){
-        return "home-logged-out";
-    }
-
-    @GetMapping("/home-logged-in")
-    public String homePageLoggedIn(){
-        return "home-logged-in";
+        return "home";
     }
 }
