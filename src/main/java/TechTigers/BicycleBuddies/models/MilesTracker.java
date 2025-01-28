1eabe5a3f3dbe909c7f8820f2064d6a528a9bdcf
Erica V <132383093+ericavalera@users.noreply.github.com>
2025-01-27 19:40:02 -0500

    added update tracker method

--- src/main/java/TechTigers/BicycleBuddies/models/MilesTracker.java
+++ src/main/java/TechTigers/BicycleBuddies/models/MilesTracker.java
@@ -1,54 +1,82 @@
 package TechTigers.BicycleBuddies.models;
 
+import jakarta.persistence.Entity;
+
+import java.util.LinkedList;
 import java.util.Objects;
+import java.util.Queue;
+
 //TODO: SERVICE LAYER
 //TODO : NEEDS METHODS FOR MONTHLY TOTAL CALCULATION AND TOTAL MILES
-//@Entity
+@Entity
 public class MilesTracker extends AbstractEntity{
-
-    // List<Rides> rides = new HashMap()<>;
-    int numofRides;
-    int milesEntered; //miles entered by user
-    int milesMonthly;
-    int milesTotal;
+    private Queue<Integer> dayTotal = new LinkedList<>();
+    private int numofRides = 0;
+    private int milesEntered = 0;
+    private int milesMonthly = 0;
+    private int milesTotal = 0;
+    private final int MONTH = 30;
 
 
     public MilesTracker(){}
 
-    public MilesTracker(int numofRides, int milesEntered, int milesMonthly, int milesTotal) {
+    public MilesTracker(Queue<Integer> dayTotal, int numofRides, int milesEntered, int milesMonthly, int milesTotal) {
+        this.dayTotal = dayTotal;
         this.numofRides = numofRides;
         this.milesEntered = milesEntered;
         this.milesMonthly = milesMonthly;
         this.milesTotal = milesTotal;
     }
 
+    public Queue<Integer> getDayTotal() {
+        return dayTotal;
+    }
 
+    public int getNumofRides() {
+        return numofRides;
+    }
 
-    public int getNumofRides() {return numofRides;}
+    public void setNumofRides(int numofRides) {
+        this.numofRides = numofRides;
+    }
 
-    public void setNumofRides(int numofRides) {this.numofRides = numofRides;}
+    public int getMilesEntered() {
+        return milesEntered;
+    }
 
-    public int getMilesEntered() {return milesEntered;}
+    public void setMilesEntered(int milesEntered) {
+        this.milesEntered = milesEntered;
+    }
 
-    public void setMilesEntered(int milesEntered) {this.milesEntered = milesEntered;}
+    public int getMilesMonthly() {
+        return milesMonthly;
+    }
 
-    public int getMilesMonthly() {return milesMonthly;}
+    public void setMilesMonthly(int milesMonthly) {
+        this.milesMonthly = milesMonthly;
+    }
 
-    public void setMilesMonthly(int milesMonthly) {this.milesMonthly = milesMonthly;}
+    public int getMilesTotal() {
+        return milesTotal;
+    }
 
-    public int getMilesTotal() {return milesTotal;}
+    public void setMilesTotal(int milesTotal) {
+        this.milesTotal = milesTotal;
+    }
 
-    public void setMilesTotal(int milesTotal) {this.milesTotal = milesTotal;}
+    public int getMONTH() {
+        return MONTH;
+    }
 
-    //TODO: needs methods for mileage
     @Override
     public String toString() {
         return "MilesTracker{" +
+                "dayTotal=" + dayTotal +
                 ", numofRides=" + numofRides +
                 ", milesEntered=" + milesEntered +
                 ", milesMonthly=" + milesMonthly +
                 ", milesTotal=" + milesTotal +
+                ", MONTH=" + MONTH +
                 '}';
     }
-
 }
--- src/main/java/TechTigers/BicycleBuddies/service/MilesTrackerService.java
+++ src/main/java/TechTigers/BicycleBuddies/service/MilesTrackerService.java
@@ -1,13 +1,14 @@
 package TechTigers.BicycleBuddies.service;
 
 import TechTigers.BicycleBuddies.data.MilesTrackerRepository;
+import TechTigers.BicycleBuddies.models.Comment;
 import TechTigers.BicycleBuddies.models.MilesTracker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+//TODO: finish service layer
 @Service
 public class MilesTrackerService {
     private final MilesTrackerRepository milesTrackerRepository;
@@ -26,5 +27,21 @@ public class MilesTrackerService {
     public Optional<MilesTracker> getTrackerById(int id){
         return milesTrackerRepository.findById(id);
     }
+
+    public MilesTracker updateTracker(int id, MilesTracker updatedTracker){
+        MilesTracker existingTracker = milesTrackerRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Tracking with ID "+ id +" does not exist."));
+        existingTracker.setNumofRides(updatedTracker.getNumofRides());
+        existingTracker.setMilesEntered(updatedTracker.getMilesEntered());
+        existingTracker.setMilesMonthly(updatedTracker.getMilesMonthly());
+        existingTracker.setMilesTotal(updatedTracker.getMilesTotal());
+        return milesTrackerRepository.save(existingTracker);
+    }
+
+    public void deleteTracker(int id){
+       milesTrackerRepository.deleteById(id);
+    }
+
+
 
 }
