2575bcafe43b482f9fe07910a1133bc6bb9ae77d
Erica V <132383093+ericavalera@users.noreply.github.com>
2025-03-15 19:11:27 -0400

    images service

--- src/main/java/TechTigers/BicycleBuddies/models/Images.java
+++ src/main/java/TechTigers/BicycleBuddies/models/Images.java
@@ -1,15 +1,47 @@
 package TechTigers.BicycleBuddies.models;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Lob;
+import lombok.Data;
 
 @Entity
+@Data
 public class Images extends AbstractEntity{
-    private String type;
+    private String contentType;
     private String name;
-
+    private String filePath;
     @Lob
-    @Column(name="imagedata", length = 1000)
     private byte[] images;
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public byte[] getImages() {
+        return images;
+    }
+
+    public void setImages(byte[] images) {
+        this.images = images;
+    }
 }
---
+++ src/main/java/TechTigers/BicycleBuddies/service/ImagesService.java
@@ -0,0 +1,47 @@
+package TechTigers.BicycleBuddies.service;
+
+import TechTigers.BicycleBuddies.data.ImagesRepository;
+import TechTigers.BicycleBuddies.models.Images;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class ImagesService {
+    @Autowired
+    private final ImagesRepository imagesRepository;
+
+    @Value("${file.upload.dir}")
+    private String uploadDir;
+
+    public ImagesService(ImagesRepository imagesRepository) {
+        this.imagesRepository = imagesRepository;
+    }
+
+    public void saveImage(MultipartFile file) throws IOException{
+        Path uploadPath = Paths.get(uploadDir);
+        if(!Files.exists(uploadPath)){
+            Files.createDirectories(uploadPath);
+        }
+
+        String fileName = UUID.randomUUID() +"_" + file.getOriginalFilename();
+        Path filePath = uploadPath.resolve(fileName);
+
+        Files.copy(file.getInputStream(), filePath);
+
+        Images images = new Images();
+        images.setName(file.getOriginalFilename());
+        images.setFilePath(filePath.toString());
+        imagesRepository.save(images);
+    }
+
+    public Images getImage(int id){
+        return imagesRepository.findById(id).orElseThrow(()-> new RuntimeException("Image not found"));
+    }
+}
